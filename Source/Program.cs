c549f5fde027883187f6c331aae5b8d0414c7e79
José Antonio Barranquero Fernández <joseantbarranquero@gmail.com>
2023-11-25 12:24:03 +0100

    clean up

--- Source/Program.cs
+++ Source/Program.cs
@@ -5,7 +5,5 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hola Mundo");
-        Console.WriteLine("Hello new world");
-        Console.WriteLine("Hello world 2");
     }
 }
